02104225329d02b28c05c3bbb7c758ea7ec714d3
Rubens Bezerra <fcorbz@gmail.com>
2026-07-28 16:48:27 -0300

    chore: remove versão local divergente do analytics BFF

--- supabase/migrations/20260727101000_public_analytics_bff_only.sql
+++
@@ -1,8 +0,0 @@
-begin;
-
-revoke execute on function public.record_site_analytics_event(uuid,text,text,jsonb)
-  from public, anon, authenticated;
-grant execute on function public.record_site_analytics_event(uuid,text,text,jsonb)
-  to service_role;
-
-commit;
